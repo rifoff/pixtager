1faa6f15216dec988e8d7c127ed87eea559a1858
Риф <rif@MacBook-Air-Rif.local>
2026-03-21 23:32:26 +0500

    feat: add Yandex Metrika

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,7 +20,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ru">
-      <body className="min-h-screen bg-bg font-sans text-txt antialiased">
+    <body className="min-h-screen bg-bg font-sans text-txt antialiased">
+        <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108182254', 'ym');
+          ym(108182254, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        `}} />
+        <noscript><div><img src="https://mc.yandex.ru/watch/108182254" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
         {children}
       </body>
     </html>
